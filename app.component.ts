13d60909b0eb9285ee17683a44d98dc6787a4c82
amiladerr <65924530+amiladerr@users.noreply.github.com>
2022-05-11 03:22:30 +0200

    Add files via upload
    ADDED - User Profile Review To Front End

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'carescoutproject';
+
+  name='Amila';
+  surname='Dervisevic';
+  age=22;
+  location='Sarajevo';
+  gender='Female';
+  height=173;
+  allergies='None';
+
+
+}
