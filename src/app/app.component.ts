425cb352a06fbc452032cae7f13e4905b3a4a73d
amiladerr <amila.derr@gmail.com>
2022-05-11 14:41:02 +0200

    Added Pages
    ADDED - Home Screen Page
    ADDED - Appropriate Variables To User

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'carescoutproject';
+  email='amila.dervisevic@stu.ssst.edu.ba';
+  name='Amila';
+  surname='Dervisevic';
+  age=22;
+  location='Sarajevo';
+  gender='Female';
+  allergies='None';
+  phonenumber='+123456789';
+
+
+}
